6dc6dc295404eb48104936f99e598685692d4af9
Zabiullah <matlabi63@gmail.com>
2024-09-30 19:37:40 +0700

    update

--- database/db.go
+++ database/db.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -19,17 +19,32 @@ func InitDatabase() {
     dbname := os.Getenv("DB_NAME")
 
     // Debug output
-    fmt.Printf("DB_USER: %s\n", user)
-    fmt.Printf("DB_PASSWORD: %s\n", password)
-    fmt.Printf("DB_HOST: %s\n", host)
-    fmt.Printf("DB_PORT: %s\n", port)
-    fmt.Printf("DB_NAME: %s\n", dbname)
+    // fmt.Printf("DB_USER: %s\n", user)
+    // fmt.Printf("DB_PASSWORD: %s\n", password)
+    // fmt.Printf("DB_HOST: %s\n", host)
+    // fmt.Printf("DB_PORT: %s\n", port)
+    // fmt.Printf("DB_NAME: %s\n", dbname)
 
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        user, password, host, port, dbname)
+    var dsn string = fmt.Sprintf(
+	    "user=%s password=%s host=%s port=%s dbname=%s",
+		user,
+		password,
+		host,
+		port,
+		dbname,
+	)
+
+    pgConfig := postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true,
+	})
 
     var err error
-    DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	DB, err = gorm.Open(pgConfig, &gorm.Config{})
+
+   
+
     if err != nil {
         panic("failed to connect to database: " + err.Error())
     }
@@ -37,58 +52,3 @@ func InitDatabase() {
     // Migrate the schema
     DB.AutoMigrate(&models.User{}, &models.Doctor{}, &models.Complaint{}, &models.Recommendation{}, &models.Information{}, &models.Admin{})
 }
-
-// package database
-
-// import (
-// 	"MentalHealthCare/models"
-// 	"MentalHealthCare/utils"
-// 	"fmt"
-// 	"log"
-
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// var (
-// 	DB_USERNAME string = utils.GetConfig("DB_USERNAME")
-// 	DB_PASSWORD string = utils.GetConfig("DB_PASSWORD")
-// 	DB_NAME     string = utils.GetConfig("DB_NAME")
-// 	DB_HOST     string = utils.GetConfig("DB_HOST")
-// 	DB_PORT     string = utils.GetConfig("DB_PORT")
-// )
-
-// func InitDB() {
-// 	var err error
-
-// 	var dsn string = fmt.Sprintf(
-// 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-// 		DB_USERNAME,
-// 		DB_PASSWORD,
-// 		DB_HOST,
-// 		DB_PORT,
-// 		DB_NAME,
-// 	)
-
-// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		log.Fatalf("error when connecting to the database: %s\n", err)
-// 	}
-
-// 	log.Println("connected to the database")
-
-// 	Migrate()
-// }
-
-// func Migrate() {
-// 	err := DB.AutoMigrate(&models.Admin{}, &models.Complaint{}, &models.Doctor{}, &models.Information{}, &models.Recommendation{}, &models.User{})
-
-// 	if err != nil {
-// 		log.Fatalf("error when migration: %s\n", err)
-// 	}
-
-// 	log.Println("migration successful")
-// }
--- main.go
+++ main.go
@@ -34,22 +34,3 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":" + port))
 }
-
-// package main
-
-// import (
-// 	"MentalHealthCare/database"
-// 	"MentalHealthCare/routes"
-
-// 	"github.com/labstack/echo/v4"
-// )
-
-// func main() {
-// 	database.InitDB()
-
-// 	e := echo.New()
-
-// 	routes.SetupRoutes(e)
-
-// 	e.Logger.Fatal(e.Start("1323"))
-// }
